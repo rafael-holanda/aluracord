212d8a3e3ddb994fe5355750be7a27a86e86301c
Rafael Holanda <59671898+rafael-holanda@users.noreply.github.com>
2022-01-25 19:33:03 +0000

    Adição de uma função GlobalStyle() básica, e a sua respectiva aplicação na HomePage()

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a1595f6b8927c4d8"; }
+/******/ 		__webpack_require__.h = function() { return "33084593e55c1224"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+function GlobalStyle(){
+	return(
+		<style global jsx>{`
+			*{
+				margin: 0;
+				padding: 0;
+				box-sizing: border-box;
+				background: #a1a1a1;
+			}
+			body{
+				font-family: sans-serif;
+			}
+		`}</style>
+	)
+}
+
 function Title(props){
 	// "props" é a propriedade de Title (vai pegar o que estiver dentro de <Title></Title>)
 	const Tag = props.tag;
@@ -20,6 +36,8 @@ function HomePage(){
 	// JSX
     return(
 		<div>
+			<GlobalStyle />
+			{/*Tag para criar estilo geral para a página*/}
 			<Title tag="h2">Boas vindas!!</Title>
 			<h2>Discord - Alura Matrix</h2>
 		</div>
